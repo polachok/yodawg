a62b54b446acd98da10969913a52fa86d20e8820
Alexander Polakov <plhk@sdf.org>
2013-03-22 17:29:54 +0400

    Start Variables section decoding

---
+++ src/Data/Binary/Format/DWG/HeaderVariables.hs
@@ -0,0 +1,18 @@
+module HeaderVariables where
+import Data.Binary
+import Types
+
+data Variables = Variables deriving (Show)
+
+instance Binary Variables where
+   put = undefined
+   get = do
+      let begginningSentinel = [0xcf, 0x7b, 0x1f, 0x23,
+                                0xfd, 0xde, 0x38, 0xa9,
+                                0x5f, 0x7c, 0x68, 0xb8,
+                                0x4e, 0x6d, 0x33, 0x5f]
+      sent <- mapM (const (get :: Get Word8)) [1..16]
+      if sent == begginningSentinel
+         then return ()
+         else fail "wrong sentinel"
+      return Variables

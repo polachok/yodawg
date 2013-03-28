e5d9fa76ed451b73df4da1b6568d6d9b9ff4a9a0
Alexander Polakov <plhk@sdf.org>
2013-03-28 04:59:11 +0400

    Add BitCoded typeclass

---
+++ src/Data/Binary/Format/DWG/Bitcoded.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+module Bitcoded where
+import Control.Applicative
+
+import Data.Binary.Bits
+import Data.Binary.Bits.Get
+import qualified Data.Binary.Get as Binary
+import Data.Binary.IEEE754
+
+import Types
+
+class Bitcoded a where
+    get :: BitGet a
+
+instance Bitcoded DWG_H where
+    get = getHandle
+
+instance Bitcoded DWG_BS where
+    get = getBitShort
+
+instance Bitcoded DWG_BD where
+    get = getBitDouble
+
+instance Bitcoded DWG_BL where
+    get = getBitLong
+
+instance Bitcoded DWG_T where
+    get = getText
+
+instance Bitcoded DWG_B where
+    get = getBool
+
+instance Bitcoded DWG_RD where
+    get = DWG_RD <$> Binary.runGet getFloat64le <$> getLazyByteString' 8
+
+instance Bitcoded DWG_2RD where
+    get = (,) <$> get <*> get
+
+instance Bitcoded DWG_3BD where
+    get = (,,) <$> get <*> get <*> get
+
+instance Bitcoded DWG_RC where
+    get = getWord8 8
+
+instance Bitcoded DWG_CMC where
+    get = DWG_CMC <$> get <*> get <*> get
